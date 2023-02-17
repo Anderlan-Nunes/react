5e5325377da423ce72a474c217882bbe959ba2a6
Anderlan pc1 <anderlan.dev@gmail.com>
2023-02-17 11:20:13 -0300

    criando os metodos das operações da calculadora

--- calculadora/src/components/Button.jsx
+++ calculadora/src/components/Button.jsx
@@ -1,9 +1,23 @@
 import './Button.css'
 
 export default props => {
-   
+   let classes = 'button ';
+   classes += props.operation ? 'operation' : '';
+   classes += props.double ? 'double' : '';
+   classes += props.triple ? 'triple' : '';
 
-    return(
+   return(
+    <button 
+        onClick={e => props.click && props.click(props.label) /* props.click(e.target.innerHTML)} */} 
+        className={classes}>
+            {props.label}
+    </button>
+   )
+
+}
+
+/**
+ *  return(
         <button className={`button
         ${props.operation ? 'operation' : ''}
         ${props.double ? 'double' : ''}
@@ -12,4 +26,11 @@ export default props => {
             {props.label}
         </button>
     )
-}
+ */
+
+/**onClick={e => props.click && props.click(e.target.innerHTML)} usou os 2 para nao dar erro pq faz essa verificaçao props.click antes de chamar a funcao click
+ * 
+ * espero receber no clique do botao uma propriedade chamada clique e esta propriedade o conteudo dela vai ser uma funcao q sera chamada so que esta funcao click em vez de receber o evento vai receber o conteudo do elemento html
+ * 
+ * props.click(e.target.innerHTML)} trocou por props.click(props.label)} o conteudo do botão eh a mesma coisa que props.label
+ */
--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -6,27 +6,46 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 
 export default class Calculator extends Component {
+
+    constructor(props) { /*pode usar assim no lugar das arrow function por causa do this */
+        super(props)
+
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+
+    clearMemory(){
+        console.log('limpar')
+    }
+
+    setOperation(operation){
+        console.log(operation)
+    }
+
+    addDigit(n){
+        console.log(n)
+    }
     render() {
         return (
             <div className='calculator'>
                 <Display displayValue={10000}/>
-                <Button label='AC'triple/>
-                <Button label='/' operation/>
-                <Button label='7'/>
-                <Button label='8'/>
-                <Button label='9'/>
-                <Button label='X' operation/>
-                <Button label='4'/>
-                <Button label='5'/>
-                <Button label='6'/>
-                <Button label='-' operation/>
-                <Button label='1'/>
-                <Button label='2'/>
-                <Button label='3'/>
-                <Button label='+' operation/>
-                <Button label='0' double/>
-                <Button label='.'/>
-                <Button label='=' operation/>
+                <Button label='AC'triple click={() => this.clearMemory()/*usa arrow function por causa do this */}/> 
+                <Button label='/' operation click={this.setOperation}/>
+                <Button label='7' click={this.addDigit}/>
+                <Button label='8' click={this.addDigit}/>
+                <Button label='9' click={this.addDigit}/>
+                <Button label='X' operation click={op => this.setOperation(op)}/>
+                <Button label='4' click={this.addDigit}/>
+                <Button label='5' click={this.addDigit}/>
+                <Button label='6' click={n => this.setOperation(n)}/>
+                <Button label='-' operation click={this.setOperation}/>
+                <Button label='1' click={this.addDigit}/>
+                <Button label='2' click={this.addDigit}/>
+                <Button label='3' click={this.addDigit}/>
+                <Button label='+' operation click={this.setOperation}/>
+                <Button label='0' click={this.addDigit} double/>
+                <Button label='.' click={this.addDigit}/>
+                <Button label='=' operation click={this.setOperation}/>
 
             </div>
         )
