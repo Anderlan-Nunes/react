da42ac8c6e5fb6f9639fa98f9291a6242eb04cac
Anderlan pc1 <anderlan.dev@gmail.com>
2023-02-25 00:08:47 -0300

    roteamento simples

--- navegacao/src/components/layout/Content.jsx
+++ navegacao/src/components/layout/Content.jsx
@@ -1,9 +1,25 @@
-import './Content.css'
+import "./Content.css";
+
+ import React from "react";
+ import { Routes, Route } from "react-router-dom"; // Importe esses arquivos
+ 
+import Home from "../../views/examples/Home";
+ //import Param from "../../views/examples/Param";
+ import About from "../../views/examples/About";
+ //import NotFound from "../../views/examples/NotFound";
 
 const Content = props => (
     <main className='Content'>
-        <h1>Component Content</h1>
+        <Routes>
+            <Route path="/about" element={<About />} />
+            
+            <Route path="/" exact element={<Home />} />
+            
+        </Routes>
     </main>
 )
 
 export default Content;
+
+//<Route path="/param/:id" element={<Param />} />
+//<Route path="*" element={<NotFound />} />
