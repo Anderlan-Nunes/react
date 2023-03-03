189f9b95729846fe6951fb8fa6087681a5860458
Anderlan pc1 <anderlan.dev@gmail.com>
2023-03-03 11:40:58 -0300

    controlar a quantidade de vezes q o component e renderizado

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -1,15 +1,67 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
 
 const UseRef = (props) => {
+
+    const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
+    const count = useRef(0)
+    const count2 = useRef(0)
+
+    useEffect(function () {
+        count2.current = count2.current + 1
+    }, [value1, value2])
+
+    count.current = count.current + 1
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <div>
+                    <span className="text">Valor: </span>
+                    <span className="text">{value1} [</span>
+                    <span className="text red">{count.current}</span>
+                    <span className="text">]</span>
+                </div>
+                <div>
+                    <span className="text">Valor2: </span>
+                    <span className="text">{value1} [</span>
+                    <span className="text red">{count2.current}</span>
+                    <span className="text">]</span>
+                </div>
+                <input
+                    type="text" className="input"
+                    value={value1}
+                    onChange={e => setValue1(e.target.value)}
+                />
+            </div>
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <input
+                    type="text" className="input"
+                    value={value2}
+                    onChange={e => setValue2(e.target.value)}
+                />
+            </div>
+
         </div>
     )
 }
 
 export default UseRef
+
+/*
+useRef()
+se eu sair e voltar ele zera pq eu vou receber um novo objeto mas enquanto eu estiver renderizando o mesmo objeto ele vai sempre retornar a mesma referência (por isso q posso colocar no corpo do componente) ou seja eu tenho um objeto ele me dá uma referencia e eu consigo mecher no atributo “current” 
+
+useRef() x useState 
+a diferença é que o useState sempre que modificar o estado o useState automaticamente o componente é renderizado automaticamente
+
+*/
+
